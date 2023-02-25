b9456178eddac1d7306c9ef3ba6a9f960e149bc6
brayanroa <brayanandresrl@ufps.edu.co>
2023-02-24 23:53:45 -0500

    added angular fox features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//* MODULOS PROPIOS
+import { MainModule } from './main/main.module';
+import { AuthModule } from './auth/auth.module';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { es_ES } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconsProviderModule } from './icons-provider.module';
+
+registerLocaleData(es);
 
 @NgModule({
   declarations: [
@@ -10,9 +25,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    IconsProviderModule,
+    MainModule,
+    AuthModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: es_ES }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
