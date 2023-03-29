42b2e3a3a7a3226bd342305805e880fda3bf20fb
Brayan Roa <brayanandresrl@ufps.edu.co>
2023-03-28 21:47:00 -0500

    feat: added base information to display

--- src/app/administration/persons/pages/see-person/see-person.component.ts
+++ src/app/administration/persons/pages/see-person/see-person.component.ts
@@ -12,42 +12,39 @@ import { MenuItem } from 'primeng/api';
 })
 export class SeePersonComponent extends BaseComponent {
 
-  title:string = ""
-  person!:OnePersonsInterface
-  loading:boolean = false
-  code:string
+  person!: OnePersonsInterface
+  loading: boolean = true
+  code: string
 
   items!: MenuItem[];
   home!: MenuItem;
 
   constructor(
-    private readonly personService:PersonService,
-    private aRoute:ActivatedRoute
-  ){
+    private readonly personService: PersonService,
+    private aRoute: ActivatedRoute
+  ) {
     super()
     this.code = this.aRoute.snapshot.paramMap.get('code') || ""
     this.uploadPerson(this.code)
   }
 
-  ngOnInit(): void {
-   
-  }
-
-  uploadPerson(code:string){
+  uploadPerson(code: string) {
     this.personService.seePerson(code).subscribe({
-      next: value =>{
-        const rol = (value.data.role.name === "docente")?"Docente":"Estudiante"
+      next: value => {
+        const rol = (value.data.role.name === "docente") ? "Docente" : "Estudiante"
         this.person = value.data
+        console.log(this.person);
         this.items = [
-          { label: "Personas", disabled:true},
-          { label: `${rol}`, routerLink: `/dashboard/${value.data.role.name}s`},
-          { 
-            label: `${value.data.names} ${value.data.lastnames}`, 
-            disabled:true
+          { label: "Personas", disabled: true },
+          { label: `${rol}`, routerLink: `/dashboard/${value.data.role.name}s` },
+          {
+            label: `${value.data.names} ${value.data.lastnames}`,
+            disabled: true
           }];
         this.home = { icon: 'pi pi-home', routerLink: '/' };
+        this.loading = false
       },
-      error: err =>{
+      error: err => {
         this.alertError(err.error.data)
       }
     })
