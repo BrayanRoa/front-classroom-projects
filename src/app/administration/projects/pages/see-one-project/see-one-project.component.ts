797d3438b902516e97ea83f6aadaf3494c0278dc
Brayan Roa <brayanandresrl@ufps.edu.co>
2023-04-01 16:24:37 -0500

    feat: loading change

--- src/app/administration/projects/pages/see-one-project/see-one-project.component.ts
+++ src/app/administration/projects/pages/see-one-project/see-one-project.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { MenuItem } from 'primeng/api';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MenuItem, Message } from 'primeng/api';
 import { ProjectService } from '../../service/project.service';
 import { DataProject } from '../../interfaces/one-project.interface';
+import { PersonService } from '../../../persons/service/person.service';
+import { BaseComponent } from '../../../../shared/base/base.component';
+import { PersonProject } from '../../interfaces/person_with_project.interface';
 @Component({
   selector: 'app-see-one-project',
   templateUrl: './see-one-project.component.html',
   styleUrls: ['./see-one-project.component.css']
 })
-export class SeeOneProjectComponent {
+export class SeeOneProjectComponent extends BaseComponent {
 
   title: string = "Proyecto"
 
@@ -17,16 +20,24 @@ export class SeeOneProjectComponent {
   group_name!: string
   project_id!: string
   project!: DataProject
+  persons!: PersonProject[]
+  messages!: Message[]
 
   loading: boolean = true;
+  // loading2: boolean = false;
+
+  responsiveOptions!: any[];
 
   items!: MenuItem[];
   home!: MenuItem;
 
   constructor(
     private aRoute: ActivatedRoute,
-    private projectService: ProjectService
+    private projectService: ProjectService,
+    private personService: PersonService,
+    private router: Router
   ) {
+    super()
     this.subject_name = this.aRoute.snapshot.paramMap.get("subject")!
     this.group_name = this.aRoute.snapshot.paramMap.get("group")!
     this.group_id = this.aRoute.snapshot.paramMap.get("group_id")!
@@ -36,12 +47,37 @@ export class SeeOneProjectComponent {
 
   ngOnInit() {
     this.home = { icon: 'pi pi-home', routerLink: '/' };
+    this.responsiveOptions = [
+      {
+        breakpoint: '1024px',
+        numVisible: 3,
+        numScroll: 3
+      },
+      {
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 2
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
   }
 
   findOneProject() {
     this.projectService.findOneProject(this.project_id).subscribe({
       next: value => {
         this.project = value.data
+        this.projectService.findProjectsWithPersons(this.project_id).subscribe({
+          next: value => {
+            this.persons = value.data
+          },
+          error: e => {
+            this.messages = [{ severity: 'info', summary: 'Info', detail: `${e.error.data}` }]
+          }
+        })
         this.items = [
           { label: 'Materias', disabled: true },
           { label: 'Mis Materias', routerLink: "/dashboard/mis_materias" },
@@ -50,6 +86,41 @@ export class SeeOneProjectComponent {
           { label: `Proyecto - ${value.data.name}`, disabled: true }
         ]
         this.loading = false
+      }
+    })
+  }
+
+  signUp() {
+    const email = localStorage.getItem("email")!
+    this.personService.seePerson(email).subscribe({
+      next: value => {
+        this.personService.signUpInProject({
+          person_id: value.data.id,
+          project_id: this.project_id,
+          state: true
+        }).subscribe({
+          next: value => {
+            this.alertSuccess(value.data)
+            this.router.navigateByUrl(`/dashboard/proyectos/${this.subject_name}/${this.group_name}/${this.group_id}`)
+          },
+          error: e => {
+            this.alertError(e.error.data)
+          }
+        })
+      },
+      error: e => {
+        this.alertError(e.error.data)
+      }
+    })
+  }
+
+  approved() {
+    this.projectService.changeState(this.project_id, "in progress").subscribe({
+      next: value => {
+        this.alertSuccess(value.data)
+      },
+      error: e => {
+        this.alertError(e.error.data)
       }
     })
   }
