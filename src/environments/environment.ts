0b41d0991c61066da0813450dbc28f9091300201
Brayan Roa <brayanandresrl@ufps.edu.co>
2023-04-16 00:15:41 -0500

    change to production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    API_URL: 'http://localhost:3000/api'
-    // API_URL: 'https://classroomprojectsts-production.up.railway.app/api'
+    // API_URL: 'http://localhost:3000/api'
+    API_URL: 'https://classroomprojectsts-production.up.railway.app/api'
 };
